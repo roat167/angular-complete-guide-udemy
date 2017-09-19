153297be3aba82620770efdcc4a78acf410432be
= <khemroat@gmail.com>
2017-09-18 20:57:11 -0700

    [Updated]: Form Reactive Form asynchronou validator and form value status chance

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Observable } from 'rxjs/Observable';
 import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
@@ -16,15 +17,37 @@ export class AppComponent implements OnInit {
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
         'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]), 
-        'email': new FormControl(null, [Validators.required, Validators.email]),
+        'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmail.bind(this)),
       }),      
       'gender': new FormControl('male'),
       'hobbies': new FormArray([])
+    });
+    // this.signupForm.valueChanges.subscribe(
+    //   (value) => {console.log(value)}
+    // );
+    this.signupForm.statusChanges.subscribe(
+      (status) => console.log(status)
+    );
+    // populate Data
+    this.signupForm.setValue({
+      'userData': {
+        'username': 'Max',
+        'email': 'max@test.com'
+      },
+      'gender': 'male',
+      'hobbies': []
+    });
+
+    this.signupForm.patchValue({
+      'userData': {
+        'username': 'Anna'
+      }
     });
   }
 
   onSubmit() {
     console.log(this.signupForm);
+    this.signupForm.reset();
   }
 
   onAddHobby() {
@@ -41,4 +64,22 @@ export class AppComponent implements OnInit {
       //return {'nameIsForbidden': false};
       return null;
   }
+
+  // handle asynchronou validator
+  forbiddenEmail(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>( 
+      (resolve, reject) => {
+        setTimeout(function() {
+          if(control.value === 'test@test.com') {
+            resolve({'emailIsForbidden': true});
+          } else {
+            resolve(null);
+          }
+        }, 1500); //1,5 sec
+      }
+    );
+
+    return promise;
+  }
+
 }
